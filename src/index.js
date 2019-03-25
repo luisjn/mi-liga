e5107eea33dabc0e5c70f2385abfc2e3dba2f618
Luis Jaramillo <ljaramillonegrete@gmail.com>
2019-03-25 00:31:26 -0500

    Generar primer componente de react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class HolaMundo extends React.Component {
+    render() {
+        return <p>Hola mundo</p>
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(<HolaMundo/>, document.getElementById('root'))

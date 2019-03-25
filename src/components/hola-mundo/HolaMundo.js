45cd9538c4c24652b3aca03128e81c14bfdc6390
Luis Jaramillo <ljaramillonegrete@gmail.com>
2019-03-25 01:16:28 -0500

    Usar estilos en los componentes

--- src/components/hola-mundo/HolaMundo.js
+++ src/components/hola-mundo/HolaMundo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './HolaMundo.css';
 
 class HolaMundo extends React.Component {
     render() {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
-import HolaMundo from './components/HolaMundo';
+import HolaMundo from './components/hola-mundo/HolaMundo';
 
 render(<HolaMundo nombre='Luis' edad='23' />, document.getElementById('root'));
 

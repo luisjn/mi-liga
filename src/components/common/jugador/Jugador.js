78b56ae35eb01c52239319db59c44f7816a91304
Luis Jaramillo <ljaramillonegrete@gmail.com>
2019-03-25 03:01:29 -0500

    Agregar proyecto base de mi-liga

---
+++ src/components/common/jugador/Jugador.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+// Material
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+// Estilos
+import './Jugador.css';
+
+class Jugador extends Component {
+    render() {
+        return (
+            <div>
+                <Card className="card">
+                    <CardMedia
+                        className="media"
+                        image={this.props.foto}
+                        title="Felinos"
+                    />
+                    <CardContent>
+                        <Typography variant="subheading" component="h2" align="center" color="primary">
+                            {this.props.nombre}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </div>
+        );
+    }
+}
+
+export default Jugador;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+
+// Material
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+// Estilos
 import './index.css';
 
-import HolaMundo from './components/hola-mundo/HolaMundo';
+// Componentes
+import Header from './components/common/header/Header';
+import Liga from './components/liga/Liga';
+import Calendario from './components/calendario/Calendario';
 
-render(<HolaMundo nombre='Luis' edad='23' tipo='nota' />, document.getElementById('root'));
+// The CODE!
+const Root = () => {
+    return (
+        <div>
+            <CssBaseline />
+            <Header />
+            <div className="contenedor">
+                <Liga />
+                <Calendario />
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 serviceWorker.unregister();

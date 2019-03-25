77e3a444bbee271154a3f7b280c0097e47c8b6b7
Luis Jaramillo <ljaramillonegrete@gmail.com>
2019-03-25 01:54:20 -0500

    Usar componentes de material-ui

--- src/components/hola-mundo/HolaMundo.js
+++ src/components/hola-mundo/HolaMundo.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+
 import './HolaMundo.css';
 
 class HolaMundo extends React.Component {
@@ -7,6 +9,7 @@ class HolaMundo extends React.Component {
             <div>
                 <h1>Hola {this.props.nombre} </h1>
                 <h2> {this.props.edad} </h2>
+                <Button variant='raised' color='primary'>Enviar</Button>
                 <p className={ this.props.tipo }>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                 Veritatis consequuntur repellendus, maxime eveniet aliquid 
                 architecto vitae pariatur exercitationem deleniti eligendi quia, 

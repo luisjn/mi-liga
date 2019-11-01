853ea4e40b96122a97a78db58e5ad5d05e57441f
Luis Jaramillo <ljaramillonegrete@gmail.com>
2019-11-01 02:14:14 -0500

    Crear archivos para mandar a producción

---
+++ dist/precache-manifest.067d33efb4ddbd19d6ea78d277704f43.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fe9d27494f3f26700bf6fd576f24721",
+    "url": "/static/media/ivan.4fe9d274.jpg"
+  },
+  {
+    "revision": "5de637058637d048febd",
+    "url": "/static/css/main.c005a1d9.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8888309aa1f117435857",
+    "url": "/static/js/2.d1247d7f.chunk.js"
+  },
+  {
+    "revision": "ec070ff55ca2286ff533cfd7cb1689ae",
+    "url": "/static/media/felinos.ec070ff5.png"
+  },
+  {
+    "revision": "e7b4175e4db9c3db98f5ae92e19bf5c8",
+    "url": "/static/media/piratas.e7b4175e.png"
+  },
+  {
+    "revision": "9752a81329c2504d71ced85663f5e70e",
+    "url": "/static/media/tiburones.9752a813.png"
+  },
+  {
+    "revision": "db573ac5894b52557a5e624b1f8d66bf",
+    "url": "/static/media/mi-liga-2.db573ac5.png"
+  },
+  {
+    "revision": "939507d89b1546ccbb15563b6d66cbdb",
+    "url": "/static/media/mi-liga.939507d8.png"
+  },
+  {
+    "revision": "392f84eb322df4c270e94b351110f16b",
+    "url": "/static/media/carolina.392f84eb.png"
+  },
+  {
+    "revision": "6b577104bb5726919dab8cff25851555",
+    "url": "/static/media/ernesto.6b577104.png"
+  },
+  {
+    "revision": "4fe9d27494f3f26700bf6fd576f24721",
+    "url": "/static/media/hugo.4fe9d274.png"
+  },
+  {
+    "revision": "5de637058637d048febd",
+    "url": "/static/js/main.14dcb48e.chunk.js"
+  },
+  {
+    "revision": "b4a99d0159a6ec68cd91a4465369a905",
+    "url": "/static/media/jose.b4a99d01.png"
+  },
+  {
+    "revision": "cc9e20dc36b0cf4c5bfda95a0a8493db",
+    "url": "/static/media/juan.cc9e20dc.png"
+  },
+  {
+    "revision": "392f84eb322df4c270e94b351110f16b",
+    "url": "/static/media/karen.392f84eb.png"
+  },
+  {
+    "revision": "392f84eb322df4c270e94b351110f16b",
+    "url": "/static/media/laura.392f84eb.png"
+  },
+  {
+    "revision": "760178851d8c4d3ff93da020fc619656",
+    "url": "/static/media/luis.76017885.png"
+  },
+  {
+    "revision": "567afa70e3eb93737f13a9c0f3f232cd",
+    "url": "/static/media/manuel.567afa70.png"
+  },
+  {
+    "revision": "392f84eb322df4c270e94b351110f16b",
+    "url": "/static/media/mariana.392f84eb.jpg"
+  },
+  {
+    "revision": "392f84eb322df4c270e94b351110f16b",
+    "url": "/static/media/paco.392f84eb.png"
+  },
+  {
+    "revision": "eb85c8dc5ab5026ac7119e828f12a4de",
+    "url": "/static/media/sergio.eb85c8dc.png"
+  },
+  {
+    "revision": "392f84eb322df4c270e94b351110f16b",
+    "url": "/static/media/susana.392f84eb.png"
+  },
+  {
+    "revision": "a759522b1d417e33ef46cd9012e90a38",
+    "url": "/index.html"
+  }
+];
